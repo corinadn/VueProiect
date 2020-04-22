fb5c246b61c93ab3dc077a869db269f61e33c85b
corinadn <corinnad147@gmail.com>
2020-04-22 14:24:47 +0300

    First commit

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <nav>
+      <Navbar/>
+    </nav>
+    <h1 class="p-3">Configurator pachet turistic</h1>
+    <div class="row">
+      <Cards/>
+    </div>
+    <form>
+      <Selects/>
+    </form>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+import Selects from '@/components/Selects.vue'
+import Cards from '@/components/Cards.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Navbar,
+    Selects,
+    Cards
+  }
+}
+</script>
